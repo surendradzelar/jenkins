b70738b29660f23a9924568ab8b450ecd9ff7ce3
surendradudi <surendradudi@gmail.com>
2021-05-27 10:36:42 +0530

    first

--- seed.groovy
+++ seed.groovy
@@ -2,3 +2,93 @@ folder('CI Pipelines'){
     displayName ('CI Pipelines')
     description ('CI Pipelines')
 }
+pipelineJob('CI Pipelines/frontend-ci'){
+     configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/dev0ps01/'+j+'.git')
+
+                                   }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }
+
+pipelineJob('CI Pipelines/users-ci'){
+     configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/dev0ps01/'+j+'.git')
+
+                                   }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }
+
+pipelineJob('CI Pipelines/login-ci'){
+     configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/dev0ps01/'+j+'.git')
+
+                                   }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }
+
+
+
+
+pipelineJob('CI Pipelines/redis-ci'){
+     configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/dev0ps01/'+j+'.git')
+
+                                   }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
+                        }
+                    }
+                }
+                'scriptPath'('jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }   
