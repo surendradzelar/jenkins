cf6156a7f2f4e5706aeb5d3d254d58bedc66c7e7
surendradudi <surendradudi@gmail.com>
2021-05-29 12:19:42 +0530

    first

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,4 +1,4 @@
-def nexus(HELLO)
+def nexus()
 {command = "curl -v -u admin:zelar123 --upload-file frontend.zip http://172.31.0.171:8081/repository/frontend/frontend1.zip"
 
 def execute_state=sh(returnStdout: true, script: command)}
