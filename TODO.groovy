040b80af20581667773a7ea514a349fae46450fe
surendradudi <surendradudi@gmail.com>
2021-05-29 12:01:28 +0530

    first

---
+++ TODO.groovy
@@ -0,0 +1,32 @@
+def call(){
+    pipeline {
+    agent {
+        label "SLAVE"
+    }
+    stages {
+        stage('Download ') {
+            steps {
+                sh '''
+                 npm install
+                 npm run build
+                '''
+            }
+        }
+        stage('seeing artifacts') {
+            steps {
+                sh '''
+                zip -r frontend.zip *
+                '''
+            }
+        }
+        stage ('Uploading artifacts to nexus') {
+            steps {
+                script{
+                    nexus
+                }
+
+            }
+        }
+    }
+} 
+}
