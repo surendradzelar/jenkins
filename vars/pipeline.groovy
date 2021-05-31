abe9acd9c01f7a181d2cf67717c95ff24d38743a
surendradudi <surendradudi@gmail.com>
2021-05-31 11:17:56 +0530

    first

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,19 +1,20 @@
 
 def call () {
-  pipeline {
-      agent {
-          label 'SLAVE'
+
+    pipeline {
+        agent {
+            label 'SLAVE'
       }
-      stages {
-          stage('seeing artifacts') {
-              steps {
+        stages {
+            stage('seeing artifacts') {
+                steps {
                   sh '''
                     zip frontend.zip *
                     '''
             }
         }
-          stage ('Uploading artifacts to nexus') {
-              steps {
+            stage ('Uploading artifacts to nexus') {
+                steps {
                   sh '''
                    curl -v -u admin:zelar123 --upload-file frontend.zip http://172.31.6.224:8081/repository/frontend/frontend.zip
                 '''
