9dbfdc54405672be8bc06f5288f025e4f1fa08b0
surendradudi <surendradudi@gmail.com>
2021-05-31 12:30:11 +0530

    first

--- vars/Todo.groovy
+++ vars/Todo.groovy
@@ -1,13 +1,22 @@
 
-def call () {
-      pipeline {
-        agent {
-            label 'SLAVE'
-      }
+def call (Map params = [ : ]) {
+    def args = [
+        Nexus = '172.31.6.224', 
+    ]
+
+    args << params
+
+    pipeline {
+        agent any 
+
+        environment{
+            COMPONENT = "${args.COMPONENT}"
+        }
         stages {
             stage('seeing artifacts') {
                 steps {
                   sh '''
+                    echo ${COMPONENT}
                     zip frontend.zip *
                     '''
             }
