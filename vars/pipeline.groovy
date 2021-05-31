11dfa480d0d9245897cc2db99ab957102b24be71
surendradudi <surendradudi@gmail.com>
2021-05-31 11:13:32 +0530

    first

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,27 +1,21 @@
-@Library('todo') _
-import hudson.model.*
-import hudson.EnvVars
-import groovy.json.JsonSlurperClassic
-import groovy.json.JsonBuilder
-import groovy.json.JsonOutput
-import groovy.json.*
-import java.net.URL
 
-def pipeline () {
+def call () {
   pipeline {
-    agent any
-    stages {
-        stage('seeing artifacts') {
-            steps {
-                sh '''
-                zip frontend.zip *
-                '''
+      agent {
+          label 'SLAVE'
+      }
+      stages {
+          stage('seeing artifacts') {
+              steps {
+                  sh '''
+                    zip frontend.zip *
+                    '''
             }
         }
-        stage ('Uploading artifacts to nexus') {
-            steps {
-                sh '''
-                curl -v -u admin:zelar123 --upload-file frontend.zip http://172.31.6.224:8081/repository/frontend/frontend.zip
+          stage ('Uploading artifacts to nexus') {
+              steps {
+                  sh '''
+                   curl -v -u admin:zelar123 --upload-file frontend.zip http://172.31.6.224:8081/repository/frontend/frontend.zip
                 '''
 
             }
