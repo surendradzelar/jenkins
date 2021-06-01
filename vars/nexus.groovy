dcf07ad0baf3c906d26f4102c3f12ef43c8a118a
surendradudi <surendradudi@gmail.com>
2021-06-01 10:21:52 +0530

    first

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -39,7 +39,8 @@ def code_build(APP_TYPE,COMPONENT) {
         print execute_com
     }
     else if(APP_TYPE == 'GOLANG') {
-        command = " go build"
+        command = " apt update && wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local && export PATH=$PATH:/usr/local/go/bin && source ~/.profile && go version && mkdir /go && cd /go && mkdir src && cd src && git clone https://github.com/zelar-soft-todoapp/login.git && cd login && export GOPATH=/go && depmod
+ && apt install go-dep && go get && go build"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
     }
