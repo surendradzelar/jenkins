c12f212ff135f81012ab59feadb257d61cb09568
surendradudi <surendradudi@gmail.com>
2021-06-01 15:34:16 +0530

    new

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -39,7 +39,7 @@ def code_build(APP_TYPE,COMPONENT) {
         print execute_com
     }
     else if(APP_TYPE == 'GOLANG') {
-        command = " go get -d && go build"
+        command = "export GOPATH=/go && go get -d && go build"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
     }
