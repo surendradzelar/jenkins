2e675043ac41bcb1d881806b5be590fca9337c6f
surendradudi <surendradudi@gmail.com>
2021-06-01 11:28:41 +0530

    first

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -11,7 +11,7 @@ def nexus(COMPONENT) {
      println("abc${get_branch_exec}abc")
      def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
     if(APP_TYPE == "NGINX" ) {
-        command = " zip -r frontend.zip * "
+        command = " zip -r ${FILENAME} * "
         def execute_com= sh(returnStdout: true, script: command)
         print execute_com
     }
@@ -22,12 +22,12 @@ def nexus(COMPONENT) {
 
     }
     else if(APP_TYPE == "java" ) {
-        command =  "cp target/*.jar users.jar * "
+        command =  "cp target/*.jar ${FILENAME} * "
         def execute_com= sh(returnStdout: true, script: command)
         print execute_com
     }
     else if (APP_TYPE == "GOLANG") {
-        command = "zip -r login.zip * "
+        command = "zip -r ${FILENAME} * "
         def execute_com= sh(returnStdout: true, script: command)
         print execute_com
     }
