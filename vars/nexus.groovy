b12afdfe168173fd6f072c8ddec579bce4fac286
surendradudi <surendradudi@gmail.com>
2021-06-02 09:26:38 +0530

    first

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -33,17 +33,17 @@ def nexus(COMPONENT) {
     }
  }
 def code_build(APP_TYPE,COMPONENT) {
-    if(APP_TYPE == 'NODEJS') {
+    if(APP_TYPE == "NODEJS") {
         command = "npm install"
         def execute_com= sh(returnStdout: true, script: command)
         print execute_com
     }
-    else if(APP_TYPE == 'GOLANG') {
+    else if(APP_TYPE == "GOLANG") {
         command ="go get -d  && go build"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
     }
-    else if(APP_TYPE == 'JAVA') {
+    else if(APP_TYPE == "JAVA") {
         command = "mvn clean package"
         def execute_com = sh(returnStdout: true, script: command)
         print execute_com
