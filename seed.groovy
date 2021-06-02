ca742efbf0fcb4d4987908f745c1e4c5c2633c06
surendradudi <surendradudi@gmail.com>
2021-06-02 16:40:21 +0530

    first

--- seed.groovy
+++ seed.groovy
@@ -34,10 +34,19 @@ pipelineJob('CI Pipelines/frontend-ci'){
     }
 pipelineJob('CI Pipelines/users-ci'){
      configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                    'hudson.triggers.SCMTrigger' {
+                        'spec'('* * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+           }
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
                             'url'('https://github.com/zssurendra01/users.git')
 
                                    }
@@ -56,10 +65,19 @@ pipelineJob('CI Pipelines/users-ci'){
 
 pipelineJob('CI Pipelines/login-ci'){
      configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                    'hudson.triggers.SCMTrigger' {
+                        'spec'('* * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+           }
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
                             'url'('https://github.com/zssurendra01/login.git')
 
                                    }
@@ -81,10 +99,19 @@ pipelineJob('CI Pipelines/login-ci'){
 
 pipelineJob('CI Pipelines/todo-ci'){
      configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                    'hudson.triggers.SCMTrigger' {
+                        'spec'('* * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+           }
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
                             'url'('https://github.com/zssurendra01/todo.git')
 
                                    }
