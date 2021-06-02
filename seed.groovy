ebc30c9f0b523d09acc77e87e8858884167b8da1
surendradudi <surendradudi@gmail.com>
2021-06-02 16:12:46 +0530

    first

--- seed.groovy
+++ seed.groovy
@@ -2,15 +2,27 @@ folder('CI Pipelines'){
     displayName ('CI Pipelines')
     description ('CI Pipelines')
 }
-pipelineJob('CI Pipelines/frontend-ci'){
-     configure { flowdefinition ->
+def component =["frontend","users","login","todo"];
+
+for (com in 0..count) {
+   def con=component[i]
+    pipelineJob('CI-Pipelines/frontend-ci') {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                   'hudson.triggers.SCMTrigger' {
+                        'spec'('* * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+            }
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/zssurendra01/frontend.git')
-
-                       }
+                            'url'('https://github.com/zssurendra01/'+con+'.git')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
                     }
                     'branches' {
                         'hudson.plugins.git.BranchSpec' {
@@ -23,15 +35,28 @@ pipelineJob('CI Pipelines/frontend-ci'){
             }
         }
     }
-pipelineJob('CI Pipelines/users-ci'){
-     configure { flowdefinition ->
+}
+def component =["frontend","users","login","todo"];
+
+for (com in 0..count) {
+   def con=component[i]
+    pipelineJob('CI-Pipelines/users-ci') {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                   'hudson.triggers.SCMTrigger' {
+                        'spec'('* * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+            }
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/zssurendra01/users.git')
-
-                                   }
+                            'url'('https://github.com/zssurendra01/'+con+'.git')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
                     }
                     'branches' {
                         'hudson.plugins.git.BranchSpec' {
@@ -44,16 +69,28 @@ pipelineJob('CI Pipelines/users-ci'){
             }
         }
     }
+}
+def component =["frontend","users","login","todo"];
 
-pipelineJob('CI Pipelines/login-ci'){
-     configure { flowdefinition ->
+for (com in 0..count) {
+   def con=component[i]
+    pipelineJob('CI-Pipelines/login-ci') {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                   'hudson.triggers.SCMTrigger' {
+                        'spec'('* * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+            }
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/zssurendra01/login.git')
-
-                                   }
+                            'url'('https://github.com/zssurendra01/'+con+'.git')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
                     }
                     'branches' {
                         'hudson.plugins.git.BranchSpec' {
@@ -66,19 +103,29 @@ pipelineJob('CI Pipelines/login-ci'){
             }
         }
     }
+}
 
+def component =["frontend","users","login","todo"];
 
-
-
-pipelineJob('CI Pipelines/todo-ci'){
-     configure { flowdefinition ->
+for (com in 0..count) {
+   def con=component[i]
+    pipelineJob('CI-Pipelines/todo-ci') {
+        configure { flowdefinition ->
+            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                'triggers' {
+                   'hudson.triggers.SCMTrigger' {
+                        'spec'('* * * * 1-5')
+                        'ignorePostCommitHooks'(false)
+                    }
+                }
+            }
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/zssurendra01/todo.git')
-
-                                   }
+                            'url'('https://github.com/zssurendra01/'+con+'.git')
+                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                        }
                     }
                     'branches' {
                         'hudson.plugins.git.BranchSpec' {
@@ -90,4 +137,5 @@ pipelineJob('CI Pipelines/todo-ci'){
                 'lightweight'(true)
             }
         }
-    }   
+    }
+}
