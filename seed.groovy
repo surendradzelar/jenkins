839d24d77bdbe121b6c35be661abaa6f6da9b73f
surendradudi <surendradudi@gmail.com>
2021-06-02 16:35:02 +0530

    first

--- seed.groovy
+++ seed.groovy
@@ -2,24 +2,36 @@ folder('CI Pipelines'){
     displayName ('CI Pipelines')
     description ('CI Pipelines')
 }
-
-    pipelineJob('CI-Pipelines/frontend-ci') {
-        configure { flowdefinition ->
-            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-                'triggers' {
-                   'hudson.triggers.SCMTrigger' {
-                        'spec'('* * * * 1-5')
-                        'ignorePostCommitHooks'(false)
-                    }
-                }
-            }
+pipelineJob('CI Pipelines/frontend-ci'){
+     configure { flowdefinition ->
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/zssurendra01/'+j+'.git')
-                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                            'url'('https://github.com/zssurendra01/frontend.git')
+
+                       }
+                    }
+                    'branches' {
+                        'hudson.plugins.git.BranchSpec' {
+                            'name'('*/main')
                         }
+                    }
+                }
+                'scriptPath'('jenkinsfile')
+                'lightweight'(true)
+            }
+        }
+    }
+pipelineJob('CI Pipelines/users-ci'){
+     configure { flowdefinition ->
+            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                    'userRemoteConfigs' {
+                        'hudson.plugins.git.UserRemoteConfig' {
+                            'url'('https://github.com/zssurendra01/users.git')
+
+                                   }
                     }
                     'branches' {
                         'hudson.plugins.git.BranchSpec' {
@@ -33,27 +45,15 @@ folder('CI Pipelines'){
         }
     }
 
-//def COMPONENT =["frontend","users","login","todo"];
-
-for (com in 0..count) {
-   def con=component[i]
-    pipelineJob('CI-Pipelines/users-ci') {
-        configure { flowdefinition ->
-            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-                'triggers' {
-                   'hudson.triggers.SCMTrigger' {
-                        'spec'('* * * * 1-5')
-                        'ignorePostCommitHooks'(false)
-                    }
-                }
-            }
+pipelineJob('CI Pipelines/login-ci'){
+     configure { flowdefinition ->
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/zssurendra01/'+con+'.git')
-                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-                        }
+                            'url'('https://github.com/zssurendra01/login.git')
+
+                                   }
                     }
                     'branches' {
                         'hudson.plugins.git.BranchSpec' {
@@ -66,28 +66,19 @@ for (com in 0..count) {
             }
         }
     }
-}
-//def COMPONENT =["frontend","users","login","todo"];
 
-for (com in 0..count) {
-   def con=component[i]
-    pipelineJob('CI-Pipelines/login-ci') {
-        configure { flowdefinition ->
-            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-                'triggers' {
-                   'hudson.triggers.SCMTrigger' {
-                        'spec'('* * * * 1-5')
-                        'ignorePostCommitHooks'(false)
-                    }
-                }
-            }
+
+
+
+pipelineJob('CI Pipelines/todo-ci'){
+     configure { flowdefinition ->
             flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/zssurendra01/'+con+'.git')
-                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-                        }
+                            'url'('https://github.com/zssurendra01/todo.git')
+
+                                   }
                     }
                     'branches' {
                         'hudson.plugins.git.BranchSpec' {
@@ -99,40 +90,4 @@ for (com in 0..count) {
                 'lightweight'(true)
             }
         }
-    }
-}
-
-//def COMPONENT =["frontend","users","login","todo"];
-
-for (com in 0..count) {
-   def con=component[i]
-    pipelineJob('CI-Pipelines/todo-ci') {
-        configure { flowdefinition ->
-            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-                'triggers' {
-                   'hudson.triggers.SCMTrigger' {
-                        'spec'('* * * * 1-5')
-                        'ignorePostCommitHooks'(false)
-                    }
-                }
-            }
-            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-                    'userRemoteConfigs' {
-                        'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/zssurendra01/'+con+'.git')
-                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-                        }
-                    }
-                    'branches' {
-                        'hudson.plugins.git.BranchSpec' {
-                            'name'('*/main')
-                        }
-                    }
-                }
-                'scriptPath'('jenkinsfile')
-                'lightweight'(true)
-            }
-        }
-    }
-}
+    }   
