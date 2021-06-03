e7f7a799b5f1bfa109b14b77338f35e7bc13a1ba
surendradudi <surendradudi@gmail.com>
2021-06-03 16:47:01 +0530

    first

--- seed.groovy
+++ seed.groovy
@@ -12,7 +12,7 @@ pipelineJob('CI Pipelines/frontend-ci'){
                     //}
                 }
            }
-            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+           flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
                 'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                     'userRemoteConfigs' {
                         'hudson.plugins.git.UserRemoteConfig' {
