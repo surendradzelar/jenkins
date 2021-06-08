f5ab95f8a3ab46ccd9e0b5936e6b2c08538bce3b
surendradudi <surendradudi@gmail.com>
2021-06-08 13:04:06 +0530

    first

--- seed.groovy
+++ seed.groovy
@@ -65,38 +65,7 @@ pipelineJob('CI Pipelines/users-ci'){
             }
         }
     }
-
-pipelineJob('CI Pipelines/login-ci'){
-     configure { flowdefinition ->
-            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-               //'triggers' {
-            //'hudson.triggers.SCMTrigger' {
-                //'spec'('* * * * 1-5')
-                //'ignorePostCommitHooks'(false)
-              //  }
-            //}
-           }
-            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-                    'userRemoteConfigs' {
-                        'hudson.plugins.git.UserRemoteConfig' {
-                            'url'('https://github.com/zssurendra01/login.git')
-                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-
-                                   }
-                    }
-                    'branches' {
-                        'hudson.plugins.git.BranchSpec' {
-                            'name'('*/tags/*')
-                        }
-                    }
-                }
-                'scriptPath'('jenkinsfile')
-                'lightweight'(true)
-            }
-        }
-    }
-
+//
 
 
 
