572181f6653b4b3d2689aad5c4c40d35ce1676fd
surendradudi <surendradudi@gmail.com>
2021-06-23 12:10:28 +0530

    first

--- seed.groovy
+++ seed.groovy
@@ -11,11 +11,11 @@ for (i in 0..count) {
   pipelineJob("CI-Pipelines/${j}-ci") {
     configure { flowdefinition ->
       flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-        'triggers' {
-          'hudson.triggers.SCMTrigger' {
-            'spec'('*/2 * * * 1-5')
-            'ignorePostCommitHooks'(false)
-          }
+        //'triggers' {
+          //'hudson.triggers.SCMTrigger' {
+            //'spec'('*/2 * * * 1-5')
+            //'ignorePostCommitHooks'(false)
+          //}
         }
       }
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
