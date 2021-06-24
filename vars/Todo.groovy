2d3dddef40c2f95ee83b6e8bfff3e572213e1599
surendradudi <surendradudi@gmail.com>
2021-06-24 11:22:59 +0530

    first

--- vars/Todo.groovy
+++ vars/Todo.groovy
@@ -44,14 +44,14 @@
 
                 }
             }
-            stage('Depoly dev env') {
+           // stage('Depoly dev env') {
                 //when {
                 
                 //}
-                steps{
-                    sh '''ansible-playbook'''
-                }
-            }
+             //   steps{
+               //     sh '''ansible-playbook'''
+                //}
+            //}
         }
     }
 
