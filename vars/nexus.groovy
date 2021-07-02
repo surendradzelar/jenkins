480893dc74758bba0bc51ab8d3fa6db7babd348f
zssurendradzelar <surendradudi432@gmail.com>
2021-07-02 08:16:11 +0530

    first

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -2,7 +2,7 @@ def nexus(COMPONENT) {
     get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
-    command = "curl -f -v -u admin:zelar123 --upload-file ${FILENAME} http://172.31.21.155:8081/repository/${COMPONENT}/${FILENAME}"
+    command = "curl -f -v -u admin:zelar123 --upload-file ${FILENAME} http://172.31.17.105:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state = sh(returnStdout: true, script: command)
 }
  def make_artifacts(APP_TYPE,COMPONENT) {
