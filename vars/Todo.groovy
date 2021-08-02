dbcb4056b8b667bf516fc497f4545f30628dc393
zssurendradzelar <surendradudi432@gmail.com>
2021-08-02 12:51:09 +0530

    updated

--- vars/Todo.groovy
+++ vars/Todo.groovy
@@ -44,13 +44,14 @@
 
                 }
             }
-            //stage('Depoly DEV env') {
+            stage('Depoly DEV env') {
                 
-              //  steps{
-                //    sh '''build job : 'deployment pipeline'''
-                  //  parameters: [string(name:'ENV',value:'dev') string(name:'COMPONENT', value:'${COMPONENT}') string(name:'version', value:"${get_branch_exec}")]
-               // }
+                steps{
+                    sh '''build job : 'deployment pipeline'''
+                    parameters: [string(name:'ENV',value:'dev') string(name:'COMPONENT', value:'${COMPONENT}') string(name:'version', value:"${get_branch_exec}")]
+                }
             }
         }
     }
 
+ }
