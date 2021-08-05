d6cb63a5ad86b1aa1476355e7eabd947676b4194
zssurendradzelar <surendradudi432@gmail.com>
2021-08-05 13:00:56 +0530

    first

--- seed.groovy
+++ seed.groovy
@@ -23,7 +23,7 @@ for (i in 0..count) {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
               'url'('https://github.com/surendradzelar/'+j+'.git')
-              //'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
 
 
             }
