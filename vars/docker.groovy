71821ada04000a0893aeac3a6f5ec38ba791f664
surendra <surendra.d@zelarsoft.com>
2021-08-13 12:16:11 +0530

    first

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,8 +1,7 @@
  def call(Map params = [:] ) {
-     //def args = [
-       // NEXUS_IP : '172.31.15.207', 
-     //]
-     //args << params
+     def args = [
+          SLAVE_LABEL : "DOCKER"
+  ]
     pipeline {
         agent {
             label "${args.SLAVE_LABEL}"
