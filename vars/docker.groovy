aaea710afe57c0a9dce5031241fbbf4f5d7d4091
surendra <surendra.d@zelarsoft.com>
2021-08-13 14:09:30 +0530

    first

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,57 +1,56 @@
-
-def call(Map params = [:]) {
-
-  def args = [
+ def call(Map params = [:] ) {
+     def args = [
           SLAVE_LABEL : "DOCKER"
   ]
-  args << params
-
-  pipeline {
-    agent {
-      node {
-        label "${args.SLAVE_LABEL}"
-      }
-    }
-
-    triggers {
-      pollSCM('* * * * 1-5')
-    }
-
-    environment {
-      COMPONENT     = "${args.COMPONENT}"
-      PROJECT_NAME  = "${args.PROJECT_NAME}"
-      SLAVE_LABEL   = "${args.SLAVE_LABEL}"
-      APP_TYPE      = "${args.APP_TYPE}"
-    }
-
-    stages {
-
-      stage('Docker Build') {
-        steps {
-          script {
-            get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
-            env.get_branch_exec=sh(returnStdout: true, script: get_branch)
-          }
-          sh '''
-            aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 488617992296.dkr.ecr.us-east-1.amazonaws.com
-            docker build -t 488617992296.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${get_branch_exec} .
-          '''
+    pipeline {
+        agent {
+            label "${args.SLAVE_LABEL}"
         }
-      }
-
-      stage('Docker Push') {
-        steps {
-          script {
-            get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
-            env.get_branch_exec=sh(returnStdout: true, script: get_branch)
-          }
-          sh '''
-            docker push 488617992296.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${get_branch_exec}
-          '''
+    
+        environment {
+            COMPONENT ="${args.COMPONENT}"
+            //NEXUS_IP = "${args.NEXUS_IP}"
+            PROJECT_NAME = "${args.PROJECT_NAME}"
+            SLAVE_LABEL = "${args.SLAVE_LABEL}"
+            APP_TYPE    = "${args.APP_TYPE}"
         }
-      }
+        stages {
+            stage('Build code & install dependencies') {
+                steps {
+                    sh ' docker build -t local .'
+                    
 
-    }
+                    }
+                }
 
-  }
-}
+            }
+            //stage('Prepare Artifacts') {
+
+              //  steps {
+                //    script {
+                  //      prepare = new nexus()
+                    //    prepare.make_artifacts("${APP_TYPE}","${COMPONENT}")
+                    //}
+                //}
+            //}
+
+            //stage('Upload Artifacts') {
+              //  steps {
+                //    script {
+                  //      prepare = new nexus()
+                    //    prepare.nexus(COMPONENT)
+                    //}
+
+                //}
+            //}
+            //stage('Depoly DEV env') {  
+                //steps{
+                  //sh '''build job : 'DEPLOYMENT-PIPELINES'''
+                    //parameters: [string(name:'ENV',value:'dev') string(name:'COMPONENT', value:"${COMPONENT}") string(name:'version', value:"${get_branch_exec}")]
+                //}
+           //}
+        //}
+    //}
+
+ }
+ }
