13bf369a70d0d3b67856ee576afbfaee369f78bf
surendradzelar <surendra.d@zelarsoft.com>
2021-08-13 10:07:23 +0530

    first

--- seed.groovy
+++ seed.groovy
@@ -66,3 +66,52 @@ pipelineJob("DEPLOYMENT-PIPELINES") {
       }
     }
   }
+
+
+
+
+
+
+
+  folder('docker-pipelines') {
+  displayName('docker pipelines')
+  description('docker pipelines')
+}
+
+def component = ["todo", "login","users","frontend"];
+
+def count=(component.size()-1)
+for (i in 0..count) {
+  def j=component[i]
+  pipelineJob("docker-pipelines/${j}-ci") {
+    configure { flowdefinition ->
+      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+        //'triggers' {
+          //'hudson.triggers.SCMTrigger' {
+            //'spec'('*/2 * * * 1-5')
+            //'ignorePostCommitHooks'(false)
+          //}
+        //}
+      }
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/surendradzelar/'+j+'.git')
+              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+
+
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/tags*')
+            }
+          }
+        }
+        'scriptPath'('jenkinsfile-docker')
+        'lightweight'(true)
+      }
+    }
+  }
+}
---
+++ vars/docker.groovy
@@ -0,0 +1,56 @@
+ def call(Map params = [:] ) {
+     //def args = [
+       // NEXUS_IP : '172.31.15.207', 
+     //]
+     //args << params
+    pipeline {
+        agent {
+            label "${args.SLAVE_LABEL}"
+        }
+    
+        environment {
+            COMPONENT ="${args.COMPONENT}"
+            //NEXUS_IP = "${args.NEXUS_IP}"
+            PROJECT_NAME = "${args.PROJECT_NAME}"
+            SLAVE_LABEL = "${args.SLAVE_LABEL}"
+            APP_TYPE    = "${args.APP_TYPE}"
+        }
+        stages {
+            stage('Build code & install dependencies') {
+                steps {
+                    sh ' docker build -t local .'
+                    
+
+                    }
+                }
+
+            }
+            //stage('Prepare Artifacts') {
+
+              //  steps {
+                //    script {
+                  //      prepare = new nexus()
+                    //    prepare.make_artifacts("${APP_TYPE}","${COMPONENT}")
+                    //}
+                //}
+            //}
+
+            //stage('Upload Artifacts') {
+              //  steps {
+                //    script {
+                  //      prepare = new nexus()
+                    //    prepare.nexus(COMPONENT)
+                    //}
+
+                //}
+            //}
+            //stage('Depoly DEV env') {  
+                //steps{
+                  //sh '''build job : 'DEPLOYMENT-PIPELINES'''
+                    //parameters: [string(name:'ENV',value:'dev') string(name:'COMPONENT', value:"${COMPONENT}") string(name:'version', value:"${get_branch_exec}")]
+                //}
+           //}
+        //}
+    //}
+
+ }
